b76dd2a3866e50b3a76e99a2b8d2cbc2a8ba8600
Jesper Lundberg <Jeppsson@fam-lundberg.se>
2022-03-16 15:07:55 +0100

    Created two classes (Fråga and Quiz)

---
+++ quiz.py
@@ -0,0 +1,26 @@
+# Jesper Lundberg
+# TEINF20
+# Quizprogram
+
+#class Fråga:
+    #def __init__(self, fråga : str, svar : list(), facit : str):
+        #self.fråga = fråga
+        #self.svar = ["Tim", "Marcus", "Jesper", "Oliver"]
+        #self.facit ="Tim"
+
+class Fråga:
+    def __init__(self, fråga : str, svar : list(), facit : str):
+        self.fråga = fråga
+        self.svar = svar
+        self.facit = facit
+
+class Quiz:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
